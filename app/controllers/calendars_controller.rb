666b7c86a9efe88fa47c9c5d379a16e418252264
nao-gabu0445 <78404803+nao-gabu0445@users.noreply.github.com>
2021-02-22 16:14:39 +0900

    Issue4　修正版

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -16,7 +15,8 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
@@ -24,7 +24,7 @@ class CalendarsController < ApplicationController
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
+    # 例)今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
 
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,5 @@
 class Plan < ApplicationRecord
   validates :plan, presence: true
   validates :date, presence: true
-  validates :calendars, presence: true
+  
 end
